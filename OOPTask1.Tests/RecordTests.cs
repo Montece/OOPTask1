28035f08d3d1cca1867d04c87917ebcf8ee485c6
Montece <89153577794@mail.ru>
2024-06-19 18:11:15 +0300

    Добавил тест

---
+++ OOPTask1.Tests/RecordTests.cs
@@ -0,0 +1,36 @@
+﻿using OOPTask1.Model;
+using Xunit;
+
+namespace OOPTask1.Tests
+{
+    public class RecordTests
+    {
+        [Fact]
+        public void Create_Null()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Model.Record(null, 0));
+        }
+
+        [Theory]
+        [InlineData(["Привет"])]
+        [InlineData(["Hi"])]
+        public void Create_GoodValue(string wordStr)
+        {
+            var word = new Word(wordStr);
+            var model = new Model.Record(word, 1);
+
+            Assert.Equal(model.Word.Value, wordStr);
+        }
+
+        [Theory]
+        [InlineData(10, 0.1d)]
+        public void GetFrequency_GoodValue(ulong allWordsCount, ulong assertFrequency)
+        {
+            var word = new Word("test");
+            var model = new Model.Record(word, 1);
+            var frequency = model.GetFrequency(allWordsCount);
+
+            Assert.Equal(assertFrequency, frequency);
+        }
+    }
+}
